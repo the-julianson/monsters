4d6744c13259621cb276d91bf265be8c3dc20347
Julian Cuentas <juliancuentas@hotmail.com>
2020-04-23 03:09:05 -0300

    Updates

---
+++ precache-manifest.a4cd6b2568911bcc89066511e975af98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba1781c97c53e9b29424235f53d81a4",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "5da00400b33e4ba9552a",
+    "url": "/monster/static/css/main.93c457e2.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monster/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da00400b33e4ba9552a",
+    "url": "/monster/static/js/main.81b1136e.chunk.js"
+  },
+  {
+    "revision": "ce4106392ac25c68b11d",
+    "url": "/monster/static/js/runtime-main.9dda15a6.js"
+  }
+]);
